573b6022f1a7c5dd746c232bc87b3e50a00f6081
Hassan Abdulwahab <devhassan.org@gmail.com>
2024-09-24 11:33:46 +0100

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Hassan Abdulwahab on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
